d153a6614f73d8f0eb342d7119d735b739a48e23
yc <475117436@qq.com>
2019-03-28 10:43:26 +0800

    add search input

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="search">
+    <input class="search-input" type="text" placeholder="Type City">
+	</div>
+</template>
+
+<script>
+    export default {
+        name: 'CitySearch',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" type="text/stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      height: .62rem
+      width: 100%
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .3rem
+
+</style>
