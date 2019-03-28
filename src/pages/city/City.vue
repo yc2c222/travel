39acf4653999a5c5e8b8375c89eb1aa7c5cd7c8f
yc <475117436@qq.com>
2019-03-28 10:25:38 +0800

    city-router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,21 @@
+<template>
+	<city-header></city-header>
+</template>
+
+<script>
+  import CityHeader from './components/Header'
+    export default {
+        name: 'City',
+        components:{
+          CityHeader
+        },
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" type="text/stylus" scoped>
+
+</style>
