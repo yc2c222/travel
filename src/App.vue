a668b48dfcc01d1e38af74e43b8265cc1581996f
yc <475117436@qq.com>
2019-04-08 15:27:24 +0800

    finish keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <!--keep-alive标签是vue自带标签，其中的内容请求过后会保存在用户的内存中，不用重复获取-->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
